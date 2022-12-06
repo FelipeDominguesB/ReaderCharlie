f9791be951b8ae78c0394387392293957ed0de57
FelipeDominguesB <felipedom2001@hotmail.com>
2022-12-06 19:56:41 -0300

    Removed api keys and changed database rules

--- reader-ui/src/environments/environment.prod.ts
+++ reader-ui/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAzgXguMUn_8cECMHKi6yFyBFOM-tRFz7M",
-    authDomain: "readerbeta-a1026.firebaseapp.com",
-    projectId: "readerbeta-a1026",
-    storageBucket: "readerbeta-a1026.appspot.com",
-    messagingSenderId: "192415327334",
-    appId: "1:192415327334:web:c70997c09d52595ba9f9a7"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
--- reader-ui/src/environments/environment.ts
+++ reader-ui/src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAzgXguMUn_8cECMHKi6yFyBFOM-tRFz7M",
-    authDomain: "readerbeta-a1026.firebaseapp.com",
-    projectId: "readerbeta-a1026",
-    storageBucket: "readerbeta-a1026.appspot.com",
-    messagingSenderId: "192415327334",
-    appId: "1:192415327334:web:c70997c09d52595ba9f9a7"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
